f66f6d6b535c51410df8a1aba016627aa5c9e5dc
Abubakkar-ai <ABU.SIDDIQUE68@student.qcc.cuny.edu>
2025-10-10 02:33:07 +0000

    complate lab8

---
+++ lab 8/lab8_siddique.cpp
@@ -0,0 +1,89 @@
+/*
+Abu Siddique
+oct 8,2025
+lab8
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout<<"\n----- example 1 : for loop-----"<<endl;
+    // print Hello five times
+    for(int action=1; action<=5; action++){
+        cout<<"Hello "<<action<<endl;
+
+    }
+    cout<<"\n----- example 2 : for loop as a decrement counter-----"<<endl;
+    // print from 9 to 1
+    int n = 9;
+    for(n; n>=1; n--){
+        cout<<n<<"\t";
+
+    }
+    cout<<endl;
+
+    cout<<"\n----- Example 3 : for loop as an increment counter with diffrent steps-----"<<endl;
+    // print from 1 to 20 in a step of 3
+    for(int m = 1; m<=20; m+= 3){
+        cout<<m<<"\t"<<endl;
+
+    }
+    cout<<endl;
+    cout<<"\n----- Example 4: for loop with conditional statement----- "<<endl;
+    // print all numbers, between -10 and 10, that are multiple of 3
+    for(int p = -10; p<=10; p++){
+        if(p%3==0 && p!=0){
+
+        }
+    }
+    cout<<endl;
+
+    cout<<"\n----- Example 5: for loop with conditional statement----- "<<endl;
+    // count all even numbers between 5 and 20
+    int even_count = 0;
+    for(int p = 5; p<=20; p++){
+        if(p%2==0 && p !=0){
+            even_count ++;
+
+        }
+    }
+    // print result
+    cout<<"There is/are "<<even_count<<" even number/s"<<endl;
+
+    cout<<"\n----- Example 6: for loop with conditional statement----- "<<endl;
+    // add all negative numbers between -8 and 5
+    int sum_negative = 0;
+    int x = -8;
+    for(x ; x<=5; x++ ){
+        if(x<0){
+            sum_negative += x;
+            
+        }
+    }
+    // print result
+    cout<<"The total sum of negative numberss = "<<sum_negative<<endl;
+
+    cout<<"\n----- Exercise 1: decrement from 21 to -21 by steps of 5 -----"<<endl;
+    // count how many numbers are multiples of 2
+    int multiple_of_two_count = 0;
+    for(int i = 21; i >= -21; i -= 5){
+        cout<<i<<"\t";
+        if(i % 2 == 0){
+            multiple_of_two_count++;
+        }
+    }
+    cout<<"\nTotal numbers that are multiple of 2: " << multiple_of_two_count<<endl;
+
+    cout<<"\n----- Exercise 2: user-defined start, count to 30 by steps of 4 -----"<<endl;
+    int user_input = 0;
+    cout<<"Enter an initial value: ";
+    cin>>user_input;
+
+    for(int i = user_input; i <= 30; i += 4){
+        cout<<i<<"\t";
+    }
+    cout<<endl;
+    return 0;
+}
